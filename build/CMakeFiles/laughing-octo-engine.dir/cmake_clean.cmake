9f27ba2943c94f6438b7ee85cf14b4d6e7060d4d
ceckert2194 <ceckert2194@gmail.com>
2025-02-23 01:11:51 -0500

    qt set up with basic functionality for adding and deleting tasks

---
+++ build/CMakeFiles/laughing-octo-engine.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laughing-octo-engine_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/laughing-octo-engine_autogen.dir/ParseCache.txt"
+  "laughing-octo-engine_autogen"
+  "CMakeFiles/laughing-octo-engine.dir/laughing-octo-engine_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/laughing-octo-engine.dir/laughing-octo-engine_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/laughing-octo-engine.dir/src/MainWindow.cpp.obj"
+  "CMakeFiles/laughing-octo-engine.dir/src/MainWindow.cpp.obj.d"
+  "CMakeFiles/laughing-octo-engine.dir/src/main.cpp.obj"
+  "CMakeFiles/laughing-octo-engine.dir/src/main.cpp.obj.d"
+  "laughing-octo-engine.exe"
+  "laughing-octo-engine.exe.manifest"
+  "laughing-octo-engine.pdb"
+  "laughing-octo-engine_autogen/mocs_compilation.cpp"
+  "laughing-octo-engine_autogen/timestamp"
+  "liblaughing-octo-engine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laughing-octo-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/laughing-octo-engine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for laughing-octo-engine.
+# This may be replaced when dependencies are built.
